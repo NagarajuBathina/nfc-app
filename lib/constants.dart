b81b41c9eb1ffb526bf403b42425d36021e3283e
Nagaraju Bathina <113161518+NagarajuBathina@users.noreply.github.com>
2024-08-13 19:15:54 +0530

    aug-13

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class Constants{
-   static void toastMessage(
-      {required BuildContext context, required String msg,  Color? bgColor,  Color? textColor}) {
-    Fluttertoast.showToast(
-      msg: msg,
-      backgroundColor: bgColor,textColor: textColor
-    );
-  }
+ 
 
   static void showLoading(BuildContext context) {
     showDialog(
